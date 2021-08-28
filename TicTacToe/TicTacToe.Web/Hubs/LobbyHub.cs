1bed79b140f459c99143cbc8405df1a8fa2cb307
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-08-28 15:12:46 +0300

    First Commit

---
+++ TicTacToe/TicTacToe.Web/Hubs/LobbyHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Web.Hubs
+{
+    public class LobbyHub : Hub
+    {
+
+        public LobbyHub()
+        {
+        }
+        public async Task SendMessage(string message)
+        {
+            var user = this.Context.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var user = this.Context.User.FindFirst(ClaimTypes.Name).Value;
+            var message = $"{user} has joined lobby.";
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
---
+++ TicTacToe/TicTacToe.Web/wwwroot/js/LobbyJs/LobbyJs.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/LobbyHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    var p = document.createElement('p');
+    var messageP = document.createElement('p');
+    messageP.textContent = `${message}`;
+p.textContent=`${user}`;
+    document.querySelector("main>section>ul").appendChild(li);
+   li.appendChild(p);
+   li.appendChild(messageP);
+});
+
+connection.start().then(function () {
+}).catch(function (err) {
+    return console.error(err.toString());
+});
