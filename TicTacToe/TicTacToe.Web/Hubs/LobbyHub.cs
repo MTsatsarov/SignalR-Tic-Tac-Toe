f24ef29a6b1f8685f9925ade58f7b16239331ea1
MTsatsarov <M.Tsatsarov92@gmail.com>
2021-09-14 14:30:11 +0300

    Preparing game ready

--- TicTacToe/TicTacToe.Web/Hubs/LobbyHub.cs
+++ TicTacToe/TicTacToe.Web/Hubs/LobbyHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -7,21 +8,43 @@ namespace TicTacToe.Web.Hubs
 {
     public class LobbyHub : Hub
     {
-
+        private static string[] GroupsArray = new string[2];
+        private static string[] Board = new string[9];
         public LobbyHub()
         {
         }
-        public async Task SendMessage(string message)
+
+        public async Task StartGame()
         {
-            var user = this.Context.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Group("PlayerOne").SendAsync("GameReady");
+            await Clients.All.SendAsync("StartTheGame", "New Game has started !");
         }
-        //public override async Task OnConnectedAsync()
-        //{
-        //    var user = this.Context.User.FindFirst(ClaimTypes.Name).Value;
-        //    var message = $"{user} has joined lobby.";
-        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-        //}
+        public override async Task OnConnectedAsync()
+        {
+            var user = this.Context.ConnectionId;
+            if (GroupsArray[0] !=null)
+            {
+                if (GroupsArray[1] != null)
+                {
+                    return;
+                }
+                GroupsArray[1] = user;
+                await this.Groups.AddToGroupAsync(user, "PlayerTwo");
+                await this.Clients.Group("PlayerTwo").SendAsync("ConnectPlayer");
+                await this.Clients.Group("PlayerOne").SendAsync("GameAvailable");
+                return;
+            }
+            GroupsArray[0] = user;
+            await this.Groups.AddToGroupAsync(user, "PlayerOne");
+            await this.Clients.Group("PlayerOne").SendAsync("ConnectPlayer");
+
+        }
+
+        public async Task GetMove()
+        {
+
+        }
+
     }
 }
 
---
+++ TicTacToe/TicTacToe.Web/wwwroot/js/Game/Game.js
@@ -0,0 +1,51 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/LobbyHub").build();
+
+
+
+connection.start().then(function () {
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+window.onload = function () {
+    document.getElementsByClassName('gameTable')[0].addEventListener('click', MakeMove);
+    document.getElementById('StartGame').addEventListener('click', SetGameReady)
+}
+
+
+function MakeMove(ev) {
+    if (ev.target.tagName == 'BUTTON') {
+
+        //    ev.target.classList.add('FirstPl')
+    }
+
+}
+connection.on("ConnectPlayer", function () {
+
+    var ul = document.getElementById('chatUl');
+    var message = "Player Connected to the game";
+    AppendMessage(ul, message);
+
+});
+
+function AppendMessage(ul, stringMessage) {
+    var li = document.createElement('li');
+    li.textContent = stringMessage;
+    ul.appendChild(li);
+}
+connection.on('StartTheGame', function (message) {
+    var ul = document.getElementById('chatUl');
+    AppendMessage(ul, message);
+});
+function SetGameReady() {
+    connection.invoke('StartGame')
+}
+
+connection.on('GameAvailable', function () {
+    document.getElementById('StartGame').attributes.removeNamedItem('disabled');
+})
+
+connection.on('GameReady', function () {
+    document.querySelectorAll('.gameTable>tbody>tr>td>button').forEach(x => x.attributes.removeNamedItem('disabled'));
+});
